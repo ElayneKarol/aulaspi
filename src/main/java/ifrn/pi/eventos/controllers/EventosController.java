3d247f45f312bbb146c89e5705cb8f8a85cccefa
Karol <elayne.sabino@escolar.ifrn.edu.br>
2023-09-15 14:14:41 -0300

    configurando banco de dados

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -8,6 +8,16 @@ public class EventosController {
 	
 	@RequestMapping("/eventos/form")
 	public String form() {
+		System.out.println("Chamou o método form");
+		return "formEvento";
+	}
+	
+	@RequestMapping("/eventos/form/salvar")
+	public String Salvar( String nome, String local, String data, String horario) {
+		System.out.println("Nome: " + nome);
+		System.out.println("Local: " + local);
+		System.out.println("Data: " + data);
+		System.out.println("Horário: " + horario);
 		return "formEvento";
 	}
 
