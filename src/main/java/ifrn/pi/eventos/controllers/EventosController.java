e494ab4156acb6013c17589aa971e99a62c5f46f
Karol <elayne.sabino@escolar.ifrn.edu.br>
2023-10-09 11:00:38 -0300

    Cadastrando e listando convidados do evento

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ifrn.pi.eventos.models.Convidado;
 import ifrn.pi.eventos.models.Evento;
+import ifrn.pi.eventos.repositories.ConvidadoRepository;
 import ifrn.pi.eventos.repositories.EventoRepository;
 
 @Controller
@@ -20,6 +22,8 @@ public class EventosController {
 
 	@Autowired
 	private EventoRepository er;
+	@Autowired
+	private ConvidadoRepository cr;
 
 	@GetMapping("/form")
 	public String form() {
@@ -54,10 +58,31 @@ public class EventosController {
 			return md;
 		}
 
-		md.setViewName("eventos/detalhes");
 		Evento evento = opt.get();
-
+		List<Convidado> convidados = cr.findByEvento(evento);
+		md.setViewName("eventos/detalhes");
 		md.addObject("evento", evento);
+		md.addObject("convidados", convidados);
 		return md;
+	}
+
+	@PostMapping("/{idEvento}")
+	public String salvarConvidado(@PathVariable Long idEvento, Convidado convidado) {
+		
+		System.out.println("Id do Evento:" + idEvento);
+		System.out.println(convidado);
+		
+		Optional<Evento> opt = er.findById(idEvento);
+		if (opt.isEmpty()) {
+			return "redirect:/eventos";
+		}
+		
+		Evento evento = opt.get();
+		convidado.setEvento(evento);
+		
+		cr.save(convidado);
+		
+		return "redirect:/eventos/{idEvento}";
+		
 	}
 }
